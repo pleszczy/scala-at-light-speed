b54ff6c13262fc41701d7f4afd3427343a9e5d7c
Piotr <piotrkun@gmail.com>
2022-03-16 23:41:30 +0100

    playing with formatting

--- src/main/scala/scalacorefptutorial/baeldung/App.scala
+++ src/main/scala/scalacorefptutorial/baeldung/App.scala
@@ -14,10 +14,11 @@ object App {
   def monads(): Unit = {
     val value: Lazy[String] = Lazy("12")
     val value1: Lazy[String] = value.map(it => it + "1")
-    val value2: Lazy[String] = value.map(it => {
-      println(s"lazy evaluated side effect => $it")
-      it
-    })
+    val value2: Lazy[String] = value.map {
+      it =>
+        println(s"lazy evaluated side effect => $it")
+        it
+    }
     val value3: Lazy[String] = value2.flatMap(it => Lazy(it))
     println(value1.get)
     println(value2.get)
