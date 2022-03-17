7ca4bdd41802ab6b51a0aee374b3c47c312e5bda
Piotr <piotrkun@gmail.com>
2022-03-17 13:36:18 +0100

    part 4

---
+++ src/main/scala/parts/Part4.scala
@@ -0,0 +1,47 @@
+package org.scalaatlightspeed
+package parts
+
+import scala.annotation.tailrec
+import scala.collection.immutable
+import scala.collection.immutable.AbstractSeq
+import scala.util.chaining.*
+
+object Part4 {
+
+  val incA = new Function1[Int, Int] {
+    override def apply(v1: Int) = v1 + 1
+  }
+
+  val incB = new (Int => Int) {
+    override def apply(v1: Int) = v1 + 1
+  }
+
+  def incC: Function1[Int, Int] = v1 => v1 + 1
+
+  def incD: Int => Int = v1 => v1 + 1
+
+  def incE = (v1: Int) => v1 + 1
+
+  def concatA: Function2[Int, Int, String] = (v1, v2) => s"$v1$v2"
+
+  def concatB: (Int, Int) => String = (v1, v2) => s"$v1$v2"
+
+  def concatC = (v1: Int, v2: Int) => s"$v1$v2"
+
+  def higherOrderFnA = (agg: Int, fnA: Int => Int, fnB: Int => Int) => agg.pipe(fnA).pipe(fnB)
+
+  def higherOrderFnB = (agg: Int, fns: Seq[Int => Int]) => fns.reduce((a, b) => a.compose(b))(agg)
+
+  def printlnMany(x: Any*) = x.foreach(println)
+
+  @main def main(): Unit = {
+    val inc = Seq(incA, incB, incC, incD, incE)
+    val incComposed = inc.reduce((a, b) => a.compose(b))
+
+    printlnMany(incA(10), incB(11), incC(12), incD(13), incE(14))
+    printlnMany(concatA(4, 2), concatB(4, 2), concatC(4, 2))
+    printlnMany(higherOrderFnA(12, incA, incC), higherOrderFnB(12, inc))
+    println(List(10, 11, 12, 13, 14).map(incComposed).filter(_ % 2 == 0))
+    println(List(10, 11, 12, 13, 14).flatMap((it: Int) => List(incComposed(it))))
+  }
+}
